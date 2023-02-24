1e86a791cf5d8acd95010ab86ecad14f4d56758e
HannahConnolly <connollyhannah93@gmail.com>
2023-02-24 12:54:07 -0500

    removed .venv

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,9 @@
 # pages/urls.py
 from django.urls import path
-from .views import homePageView
+from .views import homePageView, firstRoute, post
 
 urlpatterns = [
     path("", homePageView, name="home"),
+    path('first', firstRoute, name='first'),
+    path('post', post, name='post')
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,8 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 # pages/views.py
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+person = {}
 
 
 def homePageView(request):
     return HttpResponse("Hello, World!")
+
+
+def firstRoute(request):
+    # person = {
+    #     'first_name': 'Jeff'
+    # }
+    return JsonResponse(person)
+
+
+def post(request):
+    person = request.validated_data
+    person = {
+        'first_name': 'Jeff'
+    }
+    return JsonResponse(person)
